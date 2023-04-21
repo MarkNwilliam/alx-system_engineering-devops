9c9e1f5541a6cd41b7bae835f837d0c5b17701c8
MarkNWiiam <nkugwamarkwiliam@gmail.com>
2023-04-21 22:50:03 +0300

    flask

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
-# Creates a file
+# Puppet manifest to create a file in /tmp
 file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+  ensure => file,
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet\n',
 }
