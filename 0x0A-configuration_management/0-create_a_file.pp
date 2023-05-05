5ef6e67267d10ebcec5622c9512c8ad8b8bc6713
MarkNWiiam <nkugwamarkwiliam@gmail.com>
2023-05-05 13:55:00 +0300

    uu

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-# Puppet manifest to create a file in /tmp
+# Creates a file
 file { '/tmp/school':
-  ensure => file,
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
-  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
